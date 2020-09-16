814a054a1004a1a5b7977038a8a41e1c3fe53628
Francis Bonacic <francisbonacic@icloud.com>
2020-09-16 13:06:43 -0700

    task 1 finished

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from './components/Form'
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
+        <Form />
       </header>
     </div>
   );
---
+++ user-app/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+
+const Form = () => {
+
+return (
+
+    <h1>This is the Form components</h1>
+
+)
+
+}
+
+
+export default Form;
