7c63a28935f14db29e635c560b02dda3e44bcdfb
Francis Bonacic <francisbonacic@icloud.com>
2020-09-16 23:07:45 -0700

    almost there..need to make a POST call

--- user-app/src/App.js
+++ user-app/src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import Form from './components/Form'
+import React, {useState, useEffect} from 'react';
+import Form from './components/Form';
+import schema from './validation/formSchema';
+import axios from 'axios';
+import * as yup from 'yup'
 import './App.css';
+import formSchema from './validation/formSchema';
+
+
+
 
 function App() {
+
+  //initial state
+
+const [formState, setFormState] = useState({
+  name: "",
+  email: "",
+  password: "",
+  checkbox: "",
+});
+
+//state for errors
+
+const [errors, setErrors] = useState({
+  name: "",
+  email: "",
+  password: "",
+  checkbox: "",
+});
+
+//button state
+
+const [buttonDisabled, setButtonDisabled] = useState(true);
+
+useEffect(() => {
+formSchema.isValid(formState).then(valid => {
+  setButtonDisabled(!valid)
+})
+},[formState]);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
-        <Form />
+        <h1>⬇️ Join Our Team! ⬇️</h1>
+        <Form 
+        formState={formState}
+        setFormState={setFormState} 
+        errors={errors}
+        setErrors={setErrors}
+        buttonDisabled={buttonDisabled}
+        setButtonDisabled={setButtonDisabled}
+        />
       </header>
     </div>
   );
--- user-app/src/components/Form.js
+++ user-app/src/components/Form.js
@@ -1,13 +1,93 @@
 import React from "react";
+import './Form.css';
+import * as yup from 'yup'
+import formSchema from '../validation/formSchema';
 
 
 
 
-const Form = () => {
+const Form = (props) => {
+
+    const formState = props.formState;
+    const setFormState = props.setFormState;
+    const errors = props.errors;
+    const setErrors = props.setErrors;
+    const buttonDisabled = props.buttonDisabled;
+    const setButtonDisabled = props.setButtonDisabled;
+
+
+    //validation
+
+const validateChange = e => {
+    yup.reach(formSchema, e.target.name)
+    .validate(e.target.value)
+    .then(valid => {
+      setErrors({
+        ...errors,[e.target.name] : ""
+      });
+    })
+  .catch(err => {
+      setErrors({
+          ...errors,[e.target.name] : err.errors[0]
+      });
+  });
+};
+
+    const inputChange = event => {
+        event.persist();
+        const newFormData = {
+          ...formState, [event.target.name] : event.target.type === 
+          "checkbox" ? event.target.checked : event.target.value
+        };
+        validateChange(event);
+        setFormState(newFormData);
+      };
+
 
 return (
 
-    <h1>This is the Form components</h1>
+    <div className="theForm">
+        <form>
+            <h2>Sign <span>your life</span> away to us!</h2><br/>
+            <input 
+            className="name" 
+            name = "name" 
+            type = "text" 
+            placeholder = "Name" 
+            value={formState.name}
+            onChange={inputChange}
+            /><br/> 
+            {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+            <input 
+            className="email" 
+            name = "email" 
+            type = "email" 
+            placeholder = "Email" 
+            value={formState.email}
+            onChange={inputChange}
+            /><br/>
+            {errors.email.length > 0 ? <p className="error">{errors.email}</p> : null}
+            <input 
+            className="password" 
+            name = "password" 
+            type = "password" 
+            placeholder = "Password" 
+            value={formState.password}
+            onChange={inputChange}
+            /><br/>
+            {errors.password.length > 0 ? <p className="error">{errors.password}</p> : null}
+            <br/>
+             I agree: <input 
+             name = "checkbox" 
+             type = "checkbox" 
+             checked={formState.checkbox}
+             onChange={inputChange} 
+             /><br/>
+            <br/> 
+            <button disabled={buttonDisabled}>Submit</button>
+
+        </form>
+    </div>
 
 )
 
